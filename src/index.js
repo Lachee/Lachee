183f8366e87b8c4ea269fa7eb21397f0fe49d991
Lachee <lachlan.h@hotmail.com>
2021-03-26 19:36:48 +1100

    basic site template

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import tippy from 'tippy.js';
+import 'tippy.js/dist/tippy.css'; // optional for styling
+import 'tippy.js/animations/scale.css';
+
+tippy.setDefaultProps({    
+    appendTo: 'parent',
+    inertia: true,
+    animation: 'scale',
+});
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const MiniCssExtractPlugin  = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use:  {
+          loader: 'babel-loader', 
+          options: {                
+            presets: ['@babel/preset-env'],
+            plugins: [
+              "@babel/plugin-proposal-class-properties",
+              "@babel/plugin-proposal-private-methods",
+              '@babel/plugin-transform-runtime'
+            ]
+          },
+        }
+      },
+      {
+        test: /\.s?[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: 'css-loader' },
+          { loader: 'sass-loader', options: { sourceMap: true } },
+        ]
+      },
+      {         
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        use: [ { loader: 'file-loader', options: { name: 'fonts/[name].[ext]' } } ]
+      }
+    ]
+  },
+  plugins: [ 
+    new MiniCssExtractPlugin({ 
+      filename: 'app.css',
+      chunkFilename: 'app.[name].css',
+    }),
+  ],
+};
