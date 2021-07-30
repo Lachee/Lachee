ee9b237e0ac36fef0794181222f1ac77f03e2b73
Lachee <Lachee@users.noreply.github.com>
2021-07-30 08:12:50 +0000

    deploy: 2ef4ed2869c30974b3b3e8c7b08934f5d32d89cf

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Opts: 
+# -w watches the build
+# -p switches to production
+
+watch=''
+mode='--mode development'
+while getopts ":wp" opt; do
+  case $opt in
+    w)
+      watch='--watch'
+      ;;
+    p)
+      mode='--mode production'
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+echo "Building $mode $watch...";
+rm -fR ./dist/
+npx webpack --config webpack.config.js $mode $watch
+echo 'Done.';
