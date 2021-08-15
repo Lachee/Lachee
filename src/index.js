db43db84df556c2f53787bc1e5a8b65e5a2c44eb
Lachee <lachlan.h@hotmail.com>
2021-08-15 19:56:06 +1000

    refactored codebase slightly

--- src/index.js
+++ src/index.js
@@ -15,13 +15,15 @@ import './mobile.js';
 // import 'tippy.js/animations/scale.css';
 
 import './window.js';
-import './projects.js';
+import { createProjectWindows } from './projects.js';
+import { createVideoFeed } from './videofeed';
 
 async function wait(duration) {
     return new Promise((resolve, reject) => {
         setTimeout(() => resolve(), duration);
     });
 }
+
 export function createTooltip(selector = '[title][^data-tippy-content]') {
     // Setup the tooltips
     $(selector).each((i, elm) => {
@@ -34,143 +36,12 @@ export function createTooltip(selector = '[title][^data-tippy-content]') {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    const preivewImageSwapDuration  = 0.5 * 1000;
-    let previewImageTimer           = 0;
-    const $rightColumn              = $('.column-right');
-    const loadedImages              = { 'example': $rightColumn.get(0) };
-    let fadeTimeout                 = null;
-    let currentSrc                  = null;
-
-    /** Marks all the elements as hidden */
-    function hideAll() {
-        
-        //Hide all other content
-        //console.log('Hiding All');
-        currentSrc = null;
-
-        // Clear existing timeouts
-        if (fadeTimeout != null) {
-            window.clearTimeout(fadeTimeout);
-            fadeTimeout = null;
-        }
-
-        // Hide everything
-        for(const key in loadedImages) {
-            $(loadedImages[key]).removeClass('visible').addClass('hidden');
-        }
-    }
-
-    /** Shows a element, otherwise returns false. */
-    function show(src) {
-        // Ensure the element exists
-        const elm = loadedImages[src];
-        if (!elm) return false;
-
-        //Make sure we are not going to the same src
-        if (currentSrc == src) return false;
-
-        //Enforce Hide all the items. This will clear the previous timeout too
-        hideAll();
-
-        //Calculate the duration its been and wait that long
-        const removalDelay = 100;
-        let duration =  preivewImageSwapDuration - (performance.now() - previewImageTimer) - removalDelay;
-        if (duration < 1) duration = 1; 
-        
-        console.log('Showing ', src, 'in', duration, 'ms');
-        currentSrc = src;
-
-        //Fade it in after some time.
-        fadeTimeout = setTimeout(() => {
-            //Display the element
-            $(elm).removeClass('hidden');
-            //document.requestAnimationFrame();
-            setTimeout(() => {
-                //console.log('Element Visible', elm, src);
-                $(elm).addClass('visible');
-            }, removalDelay);
-        }, duration);
-
-        return fadeTimeout;
-    }
-
-    // Hide everything
-    $rightColumn.on('mouseleave', (e) => { hideAll(); });
-    if (!window.isMobile()) {
-        $('.hover-box[data-image-src], .hover-box[data-video-src]').each((i, e) => {
-            const $target = $(e);
-
-            let imgSrc = $target.attr('data-image-src'); 
-            if (!imgSrc) imgSrc = $target.closest('.hover-box').attr('data-image-src');
-    
-            let videoSrc = $target.attr('data-video-src'); 
-            if (!videoSrc) videoSrc = $target.closest('.hover-box').attr('data-video-src');
-            
-            let additionalClasses = $target.attr('data-add-class'); 
-            createVideoElement(imgSrc || videoSrc, videoSrc != null, false, additionalClasses);
-        });
-    }
-
-    // Logic for the button's to show the video/gif
-    $('.hover-box[data-image-src], .hover-box[data-video-src]').on('mouseover', async (e) => {
-        //Find the hover box
-        let $target = $(e.target);
-        if (!$target.hasClass('.hover-box')) 
-            $target = $target.closest('.hover-box');
-        
-        //Get the sources
-        const imgSrc            = $target.attr('data-image-src'); 
-        const videoSrc          = $target.attr('data-video-src'); 
-        const additionalClasses = $target.attr('data-add-class'); 
-        
-        //Start the timer
-        previewImageTimer = performance.now();
-
-        const src       = imgSrc || videoSrc;
-        const isVideo   = videoSrc != null;
-
-        let element     = null;
-
-        //Hide all the items and show only the one we may have
-        if ((element = loadedImages[src]) == null) {
-            // Create the element
-            return createVideoElement(src, isVideo, true, additionalClasses);
-        }  else {
-            
-            // We can trigger the show early
-            //console.log('Shown', loadedImages);
-            show(src);
-        }
-        
-        // Target left, so lets hide the content
-        $target.on('mouseleave', async (e) => {  hideAll(); }, { once: true });
-        e.preventDefault();
-    });
-
-    /** CReate the video element if it doesn't exist */
-    function createVideoElement(src, isVideo, showAfterLoad = false, additionalClasses = []) {
-        if (loadedImages[src] != null) 
-            return loadedImages[src];
-
-        console.log('Loading video',  src, isVideo, showAfterLoad);
-        console.trace();
-
-        // Create the element
-        const $element = isVideo ? $('<video loop autoplay muted>') : $('<img>');
-        $element.attr("src", src).addClass(isVideo ? 'preview-video' : 'preview-image').addClass('loading');
-        $element.prependTo($rightColumn);
-        $element.addClass(additionalClasses);
-        
-        // The element finally loaded, so we will trigger the show
-        $element.on('load loadstart', (e) => { 
-            console.log('Loaded video', src, e.target, showAfterLoad); 
-            $element.removeClass('loading');
-            if (showAfterLoad) show(src);
-        }, { once: true });
-        
-        // Return the element
-        return loadedImages[src] = $element.get(0);
-    }
-
-    createTooltip('[title]');
+    createProjectWindows();
+    createAboutWindows();
+    createVideoFeed();
+    createTooltip('[title]');5
 }); 
+
+function createAboutWindows() {
+
+}
